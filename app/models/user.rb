5708bf23a8f570082fce9bd81cacb0f5fb46b440
rika <mameta1206@yahoo.co..jp>
2019-06-14 08:03:40 +0000

    user registration

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  #バリデーション
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  #パスワードの暗号化
+  has_secure_password 
+end
